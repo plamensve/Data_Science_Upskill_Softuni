f958efd8f8793342b8357b2303c42b8897b0f6a4
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2025-07-14 00:40:12 +0300

    Problem 9 - Exploratory / add ranking system

---
+++ 05_data_visualization_exploratory_data_analysis/ranking_sys.py
@@ -0,0 +1,15 @@
+class Ranking:
+    def __init__(self, data_views, data_likes, data_posts):
+        self.data_views = data_views
+        self.data_likes = data_likes
+        self.data_posts = data_posts
+
+    def get_mean(self):
+        mean_value = self.data_views
+        return mean_value
+
+    def second_example(self):
+        return 'Second Success'
+
+    def third_example(self):
+        return 'Third success'
